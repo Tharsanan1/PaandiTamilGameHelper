06b1a3b58beba45a5436ac172ba5f25f843bb090
tharsanan1 <tharsanan.15@cse.mrt.ac.lk>
2020-05-24 12:22:28 +0530

    added backend feature

--- src/main/java/PaandiHelper.java
+++ src/main/java/PaandiHelper.java
@@ -1,17 +1,52 @@
+import com.google.gson.Gson;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 
 public class PaandiHelper {
+    private static String PLAYER_A_VALUES = "PLAYER_A_VALUES";
+    private static String PLAYER_B_VALUES = "PLAYER_B_VALUES";
+    private static String PROCESSING_TIME = "PROCESSING_TIME";
+
+
     public static void main(String[] args) throws UnhandledLogicException, InvalidArgumentException {
-        Board board = new Board(new int[]{1,4,4,10,7,1}, new int[]{3,7,1,7,8,2,2});
+
+        JSONObject argumentObject = new JSONObject(args[0]);
+        JSONArray playerAArr = argumentObject.getJSONArray(PLAYER_A_VALUES);
+        int[] aArr = new int[playerAArr.length()];
+        JSONArray playerBArr = argumentObject.getJSONArray(PLAYER_B_VALUES);
+        int[] bArr = new int[playerBArr.length()];
+
+        for (int i = 0; i < playerAArr.length(); i++) {
+            aArr[i] = playerAArr.getInt(i);
+        }
+
+        for (int i = 0; i < playerBArr.length(); i++) {
+            bArr[i] = playerBArr.getInt(i);
+        }
+
+        int processingTime = argumentObject.getInt(PROCESSING_TIME);
+
+
+        Board board = new Board(aArr, bArr);
 //        board.playGame(Board.PLAYER_A,0);
-        Board.setToAchive(52);
-        ArrayList<Integer> arr = new ArrayList<>();
-        System.out.println("best index : " + Board.bestScoreRecursive(board, Board.PLAYER_A, arr));
-        board.printBoard();
-        int index = Board.bestScoreRecursive(board, Board.PLAYER_A, arr);
-        board.findScoreForThisStep(Board.PLAYER_A, index);
-        board.printBoard();
-        System.out.println(arr.toString());
+        //Board.setToAchive(95);
+        Board.setStartTime(new Date());
+        Board.setProcessingMaxTimeInSeconds(processingTime);
+        ArrayList<Integer> selectionChoices = new ArrayList<>();
+        int score = Board.bestScoreRecursive(board, Board.PLAYER_A, selectionChoices);
+        HashMap<Integer, ArrayList<Integer>> map = Board.getBestScoreWithLessChoicesMap();
+        Gson gson = new Gson();
+        String json = gson.toJson(map);
+        System.out.println(json);
 
     }
 }
+
+
+/**
+ "{PLAYER_A_VALUES:[7,7,7,7,7,7,7],PLAYER_B_VALUES:[7,7,7,7,7,7,7],PROCESSING_TIME:5}"
+ */
